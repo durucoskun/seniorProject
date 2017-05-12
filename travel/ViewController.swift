d329e4229ac419835e5fa06d33163174576a733c
aaygen <aygen.ata@gmail.com>
2017-05-12 16:14:24 +0300

    username outlet fix

--- travel/ViewController.swift
+++ travel/ViewController.swift
@@ -54,7 +54,7 @@ class ViewController: UIViewController ,UITableViewDelegate{
     }
     
  
-    func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
