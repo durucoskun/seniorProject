64884958f16bce538ba71192fbcc675b2efd8404
durucoskun <durucoskun@Durus-Mac.local>
2018-01-09 10:30:41 -0800

    user profile

--- travel/UserProfileController.swift
+++ travel/UserProfileController.swift
@@ -11,37 +11,48 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
-class UserProfileController: UIViewController{
+class UserProfileController: UIViewController,  UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
     
     var mail : String!
     var user: String!
     var userUid : String!
     var ref : DatabaseReference!
+    let picker = UIImagePickerController()
    
-    @IBOutlet weak var profilePic: UIImageView!
-    
+    @IBOutlet weak var profilePicView: UIImageView!
+   
     @IBAction func logout(_ sender: Any) {
         try! Auth.auth().signOut()
-        self.performSegue(withIdentifier: "logout", sender: Any?)
+        self.performSegue(withIdentifier: "logout", sender: Any?.self)
     }
     
+    @IBAction func changePic(_ sender: Any) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)    }
     
     
-    @IBOutlet weak var message: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        message.layer.masksToBounds = true
-        message.layer.cornerRadius = 8.0
-       // ref = FIRDatabase.database().reference().child("USERS").child(self.userUid).child("INTERESTS")
+        picker.delegate = self
+       ref = Database.database().reference().child("USERS").child(self.userUid).child("INTERESTS")
+        //ref.updateChildValues(["RoadTrip":1])
         
     }
-    @IBAction func chooseRoadtrip(_ sender: Any) {
-        ref.updateChildValues(["RoadTrip":1])
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        profilePicView.contentMode = .scaleAspectFit //3
+        profilePicView.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+         dismiss(animated: true, completion: nil)
     }
     
     
-    
-    
-    
+    }
     
 }
